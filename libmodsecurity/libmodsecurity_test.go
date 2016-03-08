5502b7fc90582c846829fdf54f0f832e79575504
Senghoo Kim <shkdmb@gmail.com>
2016-03-08 09:42:24 +0800

    add some test code not working

---
+++ libmodsecurity/libmodsecurity_test.go
@@ -0,0 +1,40 @@
+package libmodsecurity
+
+import (
+	"os"
+	"testing"
+)
+
+var libmodsecurity *LibModSecurity
+
+func TestVar(t *testing.T) {
+	err := libmodsecurity.AddRule("SecRule REQUEST_LINE \"@contains php\" \"id:1,phase:1,deny\"")
+	if err != nil {
+		t.Fatal("cant add rule ", err)
+	}
+	trans := libmodsecurity.NewTransaction()
+	trans.ProcessConnection("192.168.1.1", "192.168.1.2", 22345, 80)
+	trans.ProcessURL("/a.php?test=test", "GET ", 1, 1)
+	trans.ProcessRequestHeader()
+	i := trans.Intervention()
+	if i == nil {
+		t.Fatal("intervention return nil fail")
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	ret := m.Run()
+	if ret == 0 {
+		teardown()
+	}
+	os.Exit(ret)
+}
+
+func setup() {
+	libmodsecurity = NewLibModSecurity()
+
+}
+
+func teardown() {
+}
